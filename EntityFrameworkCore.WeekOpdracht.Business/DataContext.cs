af5eac9f0b52f586a80934df64939477582d98aa
Apodemus Erectus <bnkho98@hotmail.com>
2021-09-01 15:26:37 +0200

    initial commit

---
+++ EntityFrameworkCore.WeekOpdracht.Business/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using EntityFrameworkCore.WeekOpdracht.Business.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.WeekOpdracht.Business
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost,1433;Database=WeekOpdrachtTDL;User ID=sa;Password=Figlo@2016!#;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>()
+                .Property(b => b.DateTimeSend)
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
---
+++ EntityFrameworkCore.WeekOpdracht/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using EntityFrameworkCore.WeekOpdracht.Business.Entities;
+using EntityFrameworkCore.WeekOpdracht.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.WeekOpdracht.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            try
+            {
+                var saved = userService.Add(user);
+
+                return Ok(saved);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message,
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(userService.Get(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message,
+                });
+            }
+        }
+
+    }
+}
