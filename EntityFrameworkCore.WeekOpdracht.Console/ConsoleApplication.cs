046efc0529073450044ad5228a380ca09a6a6ff6
Apodemus Erectus <bnkho98@hotmail.com>
2021-09-03 11:22:26 +0200

    hlep

---
+++ EntityFrameworkCore.WeekOpdracht.Console/ConsoleApplication.cs
@@ -0,0 +1,79 @@
+﻿using EntityFrameworkCore.WeekOpdracht.Business.Entities;
+using EntityFrameworkCore.WeekOpdracht.Business.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.WeekOpdracht.Console
+{
+    public class ConsoleApplication
+    {
+        private static IUserService _userService;
+        private static IMessageService _messageService;
+        private readonly ILogger _logger;
+
+        public ConsoleApplication(ILogger<ConsoleApplication> logger, IUserService userService, IMessageService messageService)
+        {
+            _logger = logger;
+            _messageService = messageService;
+            _userService = userService;
+        }
+
+        internal void Run()
+        {
+            _logger.LogInformation("Hewwo wowld UwU!");
+
+            System.Console.WriteLine("Deleting old stuff");
+            DeleteAll();
+
+            System.Console.WriteLine("Creating new user");
+            var user = CreateUser();
+
+            System.Console.WriteLine("Creating a message from the new user");
+            CreateMessage(user);
+
+            System.Console.WriteLine("Basic test succeeded.");
+            System.Console.ReadKey();
+        }
+
+        private static void CreateMessage(User user)
+        {
+            var message = _messageService.Add(new Business.Entities.Message
+            {
+                Content = "Test bericht",
+                SenderId = user.Id,
+                Title = $"Nieuw test bericht van {user.Lastname}"
+            });
+
+            System.Console.WriteLine($"    Message with title {message.Title} created. New ID is {message.Id}");
+            Thread.Sleep(2500);
+        }
+
+        private static User CreateUser()
+        {
+            var user = _userService.Add(new Business.Entities.User
+            {
+                Email = "Test@test.nl",
+                Lastname = "De Tester",
+                Name = "Test"
+            });
+
+            System.Console.WriteLine($"    User with name {user.Name} created. New ID is {user.Id}");
+            Thread.Sleep(2500);
+
+            return user;
+        }
+        private static void DeleteAll()
+        {
+            foreach (var item in _userService.GetAll().ToList())
+                _userService.Delete(item.Id);
+
+            foreach (var item in _messageService.GetAll().ToList())
+                _messageService.Delete(item.Id);
+        }
+    }
+}
--- EntityFrameworkCore.WeekOpdracht.Console/Program.cs
+++ EntityFrameworkCore.WeekOpdracht.Console/Program.cs
@@ -1,80 +1,37 @@
 ﻿using EntityFrameworkCore.WeekOpdracht.Business;
-using EntityFrameworkCore.WeekOpdracht.Business.Entities;
 using EntityFrameworkCore.WeekOpdracht.Business.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Linq;
-using System.Threading;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace EntityFrameworkCore.WeekOpdracht.Console
 {
     class Program
     {
-        private static IUserService userService;
-        private static IMessageService messageService;
-
+        private static readonly NLog.Logger NLogger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            NLogger.Info("Hello world");
             var serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
 
             var serviceProvider = serviceCollection.BuildServiceProvider();
-            userService = serviceProvider.GetService<IUserService>();
-            messageService = serviceProvider.GetService<IMessageService>();
-
-            System.Console.WriteLine("Deleting old stuff");
-            DeleteAll();
-
-            System.Console.WriteLine("Creating new user");
-            var user = CreateUser();
-
-            System.Console.WriteLine("Creating a message from the new user");
-            CreateMessage(user);
-
-            System.Console.WriteLine("Basic test succeeded.");
-            System.Console.ReadKey();
-        }
-
-        private static void CreateMessage(User user)
-        {
-            var message = messageService.Add(new Business.Entities.Message
-            {
-                Content = "Test bericht",
-                SenderId = user.Id,
-                Title = $"Nieuw test bericht van {user.Lastname}"
-            });
-
-            System.Console.WriteLine($"    Message with title {message.Title} created. New ID is {message.Id}");
-            Thread.Sleep(2500);
-        }
-
-        private static User CreateUser()
-        {
-            var user = userService.Add(new Business.Entities.User
-            {
-                Email = "Test@test.nl",
-                Lastname = "De Tester",
-                Name = "Test"
-            });
-
-            System.Console.WriteLine($"    User with name {user.Name} created. New ID is {user.Id}");
-            Thread.Sleep(2500);
-
-            return user;
-        }
-        private static void DeleteAll()
-        {
-            foreach (var item in userService.GetAll().ToList())
-                userService.Delete(item.Id);
-
-            foreach (var item in messageService.GetAll().ToList())
-                messageService.Delete(item.Id);
+            
+            ConsoleApplication app = serviceProvider.GetService<ConsoleApplication>();
+            app.Run();
         }
 
         private static void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ConsoleApplication>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IMessageService, MessageService>();
+
+            services.AddLogging(builder => {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Information);
+                builder.AddNLog("NLog.config");
+            });
         }
     }
 }
